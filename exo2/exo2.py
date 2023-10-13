4ef58f3d36d9fd8cde86ea13a9af087a16a27b6e
Visionotup <124854703+McKingN@users.noreply.github.com>
2023-10-13 11:40:28 +0100

    Update exo2 and its test

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -1,3 +1,5 @@
+import unittest
+
 """
 Complete the solution so that it returns true if the first argument(string)
 passed in ends with the 2nd argument (also a string).
@@ -7,6 +9,10 @@ Examples:
     solution('abc', 'bc') # returns true
     solution('abc', 'd') # returns false
 """
+
+def solution(arg1 : str, arg2: str):
+    return arg1.endswith(arg2)
+
 
 """
 Create unit test using those cases:
@@ -28,3 +34,35 @@ fixed_tests_False = (
     ( "spam",    "eggs"  )
 )
 """
+
+fixed_tests_True = (
+    ("samurai", "ai"),
+    ("ninja", "ja"),
+    ("sensei", "i"),
+    ("abc", "abc"),
+    ("abcabc", "bc"),
+    ("fails", "ails"),
+)
+
+fixed_tests_False = (
+    ("sumo", "omo"),
+    ("samurai", "ra"),
+    ("abc", "abcd"),
+    ("ails", "fails"),
+    ("this", "fails"),
+    ("spam", "eggs")
+)
+
+class TestSolution(unittest.TestCase):
+    def test_true_cases(self):
+        for string, ending in fixed_tests_True:
+            with self.subTest(string=string, ending=ending):
+                self.assertTrue(solution(string, ending))
+
+    def test_false_cases(self):
+        for string, ending in fixed_tests_False:
+            with self.subTest(string=string, ending=ending):
+                self.assertFalse(solution(string, ending))
+
+if __name__ == '__main__':
+    unittest.main()
